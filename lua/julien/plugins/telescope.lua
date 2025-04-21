de2b6fed5be522ade72b1cb7023ba09609f71bb5
Julien Semaan <jusemaa@akamai.com>
2025-04-21 15:49:56 -0400

    prevent dupe tabs when opening from telescope

--- lua/julien/plugins/telescope.lua
+++ lua/julien/plugins/telescope.lua
@@ -10,6 +10,8 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local action_state = require('telescope.actions.state')
+    local Path = require('plenary.path')
     local transform_mod = require("telescope.actions.mt").transform_mod
 
     local trouble = require("trouble")
@@ -21,6 +23,34 @@ return {
         trouble.toggle("quickfix")
       end,
     })
+
+    local function open_in_tab_focus(prompt_bufnr)
+      local entry = action_state.get_selected_entry()
+      if not entry then return end
+
+      -- Get absolute path for reliable comparison
+      local filename = Path:new(entry.filename):absolute()
+      
+      -- Check all tabs and windows for this file
+      for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local buf_name = Path:new(vim.api.nvim_buf_get_name(buf)):absolute()
+          
+          if buf_name == filename then
+            -- File is already open - focus it
+            actions.close(prompt_bufnr)
+            vim.api.nvim_set_current_tabpage(tab)
+            vim.api.nvim_set_current_win(win)
+            return
+          end
+        end
+      end
+
+      -- File not open - open in new tab
+      actions.close(prompt_bufnr)
+      vim.cmd("tabedit " .. vim.fn.fnameescape(filename))
+    end
 
     telescope.setup({
       defaults = {
@@ -35,7 +65,11 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next,     -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+            ["<C-t>"] = open_in_tab_focus,
             --["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+          },
+          n = {
+            ["<C-t>"] = open_in_tab_focus,
           },
         },
       },
