3eb16b73b35e3833ccb1fe4389cd9fbf7b8d6b59
Julien Semaan <jsemaan@densify.com>
2025-12-24 11:14:26 -0500

    disable autopairs for golang

--- lua/julien/plugins/autopairs.lua
+++ lua/julien/plugins/autopairs.lua
@@ -16,6 +16,7 @@ return {
         javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
         java = false, -- don't check treesitter on java
         c = false, -- don't check treesitter on java
+        go = false,
       },
     })
 
